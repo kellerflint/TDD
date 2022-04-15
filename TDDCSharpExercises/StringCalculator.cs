8300570b4c0f7538c672c6fe045718b1c5eb3f1e
kellerflint <kflint0068@gmail.com>
2022-04-14 22:20:07 -0700

    started StringCalculator exercise

---
+++ TDDCSharpExercises/StringCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCSharpExercises
+{
+    // https://tddmanifesto.com/exercises/
+    public class StringCalculator
+    {
+        public static int Run(string inputValues)
+        {
+            if (inputValues.Length == 0) return 0;
+            if (inputValues.Length == 1) return int.Parse(inputValues);
+            var values = inputValues.Split(',');
+            return values.Aggregate(0, (sum, next) => sum += int.Parse(next));
+        }
+    }
+}
---
+++ TDDCSharpExercisesTests/StringCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDCSharpExercises;
+
+namespace TDDCSharpExercisesTests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void ReturnsZeroForEmptyString()
+        {
+            Assert.AreEqual(0, StringCalculator.Run(""));
+        }
+
+        [TestMethod]
+        public void ReturnsNumberForSingleInput()
+        {
+            Assert.AreEqual(1, StringCalculator.Run("1"));
+            Assert.AreEqual(2, StringCalculator.Run("2"));
+        }
+
+        [TestMethod]
+        public void ReturnsSumForDoubleInput()
+        {
+            Assert.AreEqual(2, StringCalculator.Run("1,1"));
+            Assert.AreEqual(5, StringCalculator.Run("2,3"));
+        }
+
+        [TestMethod]
+        public void ReturnsSumForNInputs()
+        {
+            Assert.AreEqual(3, StringCalculator.Run("1,1,1"));
+            Assert.AreEqual(10, StringCalculator.Run("1,2,3,4"));
+        }
+    }
+}
